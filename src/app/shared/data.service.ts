8f4b41b78afd4e38a170146eabc60d8b1ed9cfaf
theCapm <rmattgrimsley@gmail.com>
2021-08-09 21:55:47 -0500

    Fixed the getMessages method
    Fixed the adding of the session_id query parameter to the getMessages request. Got rid of all the junk in the getMessages method and the interceptor

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -10,39 +10,8 @@ export class DataService {
     constructor(private http: HttpClient) {}
 
     getMessages(sessionId: number) {
-        // const params = new HttpParams();
-        // params.set('session_id', `${sessionId}`);
-        // params.set('token', `${environment.apiKey}`);
-        // debugger;
-        // this.http
-        //     .get(environment.apiUrl + '/retrieve_messages', { params: params })
-        //     .subscribe((responseData) => {
-        //         debugger;
-        //         if (responseData['success']) {
-        //             console.log(responseData);
-        //         } else {
-        //             console.log('getMessages ERROR');
-        //             console.log(responseData['message']);
-        //         }
-        //     });
-        let params = new HttpParams()
-            .set('session_id', `${sessionId}`);
-            // .set('token', `${environment.apiKey}`);
-
-        debugger;
-        // apiKey: 'HALUAH08houahs08208hodnoun08n08n08n02naufnaofuna08';
-        // this.http
-        //     .get(environment.apiUrl + '/retrieve_messages?session_id=10&token=' + environment.apiKey)
-        //     .subscribe((r) => console.log(r));
-        this.http
-            .get(environment.apiUrl + '/retrieve_messages', { params: params })
-            .subscribe((r) => {
-                if (r['success']) {
-                    console.log(r);
-                } else {
-                    console.log('oops');
-                    console.log(r['message']);
-                }
-            });
+        this.http.get(environment.apiUrl + '/retrieve_messages', {
+            params: { session_id: `${sessionId}` }
+        }).subscribe((response) => console.log(response))
     }
 }
