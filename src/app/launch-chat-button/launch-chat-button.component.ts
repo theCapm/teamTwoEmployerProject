8d3cdea9d8061a759ff0a5906ded6ee6dd71cb88
theCapm <rmattgrimsley@gmail.com>
2021-08-11 22:32:17 -0500

    added launch chat button component
    Added a link styled with an absolute position to serve as a floating button for launching the chat modal.  It is a part of the navbar component (for now)

---
+++ src/app/launch-chat-button/launch-chat-button.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+    selector: 'app-launch-chat-button',
+    templateUrl: './launch-chat-button.component.html',
+    styleUrls: ['./launch-chat-button.component.scss']
+})
+export class LaunchChatButtonComponent implements OnInit {
+    constructor(private modalService: NgbModal) {}
+
+    ngOnInit(): void {}
+
+    openFormModal() {
+        /*Open the modal component*/
+        // const modalRef = this.modalService.open(
+        //     /*Component name here*/
+        // );
+
+        // modalRef.result
+        //     .then((result) => {
+        //         console.log(result);
+        //     })
+        //     .catch((error) => {
+        //         console.log(error);
+        //     });
+
+        console.log('openFormModal()!');
+    }
+}
